641cca37fa1ca1f916cf4adf28e8b2599a876ca0
drjhatka <bishawjit12@gmail.com>
2024-06-06 07:35:54 -0400

    auth configuration sealed

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,21 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB3cEfLsdCm7f07KHHeWQ30FyXonCnF9A4",
+//   authDomain: "assignment-9-9b7a0.firebaseapp.com",
+//   projectId: "assignment-9-9b7a0",
+//   storageBucket: "assignment-9-9b7a0.appspot.com",
+//   messagingSenderId: "507584066957",
+//   appId: "1:507584066957:web:b1ac56c72def2c7e2df9c4"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyB3cEfLsdCm7f07KHHeWQ30FyXonCnF9A4",
-  authDomain: "assignment-9-9b7a0.firebaseapp.com",
-  projectId: "assignment-9-9b7a0",
-  storageBucket: "assignment-9-9b7a0.appspot.com",
-  messagingSenderId: "507584066957",
-  appId: "1:507584066957:web:b1ac56c72def2c7e2df9c4"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
